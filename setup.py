a603adb51061ca0e0be9146ff99daded79e4e4f4
CarlosNasayo <CarlosNasayo@users.noreply.github.com>
2023-12-09 05:01:00 +0000

    Update version to v0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aclimate_orm",
-    version='0.0.0',
+    version='v0.0.1',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="orm for aclimate",
